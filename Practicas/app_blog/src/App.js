05f2c32739d04601d497b5f9475a2330154f637e
deathstars22 <josgabfer@outlook.com>
2021-02-12 10:17:29 -0600

    Basic blog example finished

--- Practicas/app_blog/src/App.js
+++ Practicas/app_blog/src/App.js
@@ -1,11 +1,12 @@
 import React from 'react'
 import styled from 'styled-components';
-import {BrowserRouter, Route} from 'react-router-dom';
+import {BrowserRouter, Route, Switch} from 'react-router-dom';
 import Header from './componentes/Header';
 import Blog from './componentes/Blog';
 import AcercaDe from './componentes/AcercaDe';
 import Inicio from './componentes/Inicio';
 import Post from './componentes/Post';
+import Error404 from './componentes/Error404';
 
 const App = () => {
   return (
@@ -13,10 +14,14 @@ const App = () => {
       <ContenedorPrincipal>
         <Header/>
         <Main>
-          <Route path="/" exact={true} component={Inicio}/>
-          <Route path="/blog" component={Blog}/>
-          <Route path="/post/:id" component={Post}/>
-          <Route path="/acerca-de" component={AcercaDe}/>
+          {/* Switch redirects to the last page as default */}
+          <Switch>
+            <Route path="/" exact={true} component={Inicio}/>
+            <Route path="/blog" component={Blog}/>
+            <Route path="/post/:id" component={Post}/>
+            <Route path="/acerca-de" component={AcercaDe}/>
+            <Route component={Error404}/>
+          </Switch>
         </Main>
     </ContenedorPrincipal>
     </BrowserRouter>
