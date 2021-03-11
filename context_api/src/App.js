809c169f244d0bb1c1ce61f2065aeb935db90010
deathstars22 <josgabfer@outlook.com>
2021-03-11 09:08:16 -0600

    created new app for context_api

---
+++ context_api/src/App.js
@@ -0,0 +1,44 @@
+import React from 'react'
+import styled from 'styled-components';
+import {BrowserRouter, Route, Switch} from 'react-router-dom';
+import Header from './componentes/Header';
+import Blog from './componentes/Blog';
+import AcercaDe from './componentes/AcercaDe';
+import Inicio from './componentes/Inicio';
+import Post from './componentes/Post';
+import Error404 from './componentes/Error404';
+
+const App = () => {
+  return (
+    <BrowserRouter>
+      <ContenedorPrincipal>
+        <Header/>
+        <Main>
+          {/* Switch redirects to the last page as default */}
+          <Switch>
+            <Route path="/" exact={true} component={Inicio}/>
+            <Route path="/blog" component={Blog}/>
+            <Route path="/post/:id" component={Post}/>
+            <Route path="/acerca-de" component={AcercaDe}/>
+            <Route component={Error404}/>
+          </Switch>
+        </Main>
+    </ContenedorPrincipal>
+    </BrowserRouter>
+  );
+};
+
+const ContenedorPrincipal = styled.div`
+  padding: 40px;
+  width: 90%
+  max-width: 700px;
+`;
+const Main = styled.main`
+  background: #fff;
+  padding: 40px;
+  border-radius: 10px;
+  box-shadow: 0px 0px 5px rgba(129, 129, 129, 0.1);
+`;
+
+
+export default App;
