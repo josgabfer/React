89d7768903b7a3d044964081aa70add371cd511b
Jose Duarte <josgabfer@outlook.com>
2021-03-22 17:48:36 -0600

    added required libraries, webfont, styled, and router

--- app_lista_gastos/src/index.js
+++ app_lista_gastos/src/index.js
@@ -2,6 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import WebFont from 'webfontloader';
+
+WebFont.load({
+  google: {
+    families: ['Work Sans: 400,500,700', 'sans-serif']
+  }
+});
 
 const Index = () => {
   return ( 
