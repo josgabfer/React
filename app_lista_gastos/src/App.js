1c280db53325a02a22170b09293b2faff5c37d1f
Jose Duarte <josgabfer@outlook.com>
2021-03-22 17:38:35 -0600

    Working on final React project

---
+++ app_lista_gastos/src/App.js
@@ -0,0 +1,11 @@
+import logo from './logo.svg';
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <h1>Hola Mundo</h1>
+  );
+}
+
+export default App;
---
+++ app_lista_gastos/src/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+
+const Index = () => {
+  return ( 
+    <App/>
+   );
+}
+ 
+ReactDOM.render(<Index/>,document.getElementById('root')
+);
