8da7935e48eead503b47013a659e6fa9f9b10bcc
Jose Duarte <josgabfer@outlook.com>
2021-05-06 21:52:38 -0600

    Added icons to the dropdown menu

--- app_lista_gastos/src/componentes/FormularioGastos.js
+++ app_lista_gastos/src/componentes/FormularioGastos.js
@@ -2,9 +2,11 @@ import React, {useState} from 'react';
 import {ContenedorFiltros, Formulario, Input, InputGrande, ContenedorBoton} from './../elementos/ElementosDeFormulario';
 import Boton from './../elementos/Boton';
 import {ReactComponent as IconoPlus} from './../imagenes/plus.svg' ;
+import SelectCategorias from './SelectCategorias';
 const FormularioGasto = () => {
     const [inputDescripcion, cambiarInputDescripcion] = useState('');
     const [inputCantidad, cambiarInputCantidad] = useState('');
+    const [categoria, cambiarCategoria] = useState('hogar');
 
     const handleChange = (e) =>{
         if(e.target.name === 'descripcion'){
@@ -14,12 +16,13 @@ const FormularioGasto = () => {
         }
     }
 
-
-
     return (
         <Formulario>
             <ContenedorFiltros>
-                <p>Select</p>
+                <SelectCategorias
+                    categoria={categoria}
+                    cambiarCategoria={cambiarCategoria}
+                />
                 <p>Date Picker</p>
             </ContenedorFiltros>
             <div>
