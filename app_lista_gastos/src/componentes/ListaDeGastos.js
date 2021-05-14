7ca3feef12cd35dc2bcc73e5a37f2a6c800b4222
Jose Duarte <josgabfer@outlook.com>
2021-05-14 09:28:22 -0600

    fixed date showing on lista de gastos

--- app_lista_gastos/src/componentes/ListaDeGastos.js
+++ app_lista_gastos/src/componentes/ListaDeGastos.js
@@ -10,6 +10,8 @@ import {ReactComponent as IconoEditar} from './../imagenes/editar.svg';
 import {ReactComponent as IconoBorrar} from './../imagenes/borrar.svg';
 import {Link} from 'react-router-dom';
 import Boton from './../elementos/Boton';
+import {format, fromUnixTime} from 'date-fns';
+import {es} from 'date-fns/locale';
 
 import {
   Lista,
@@ -31,6 +33,22 @@ import {
 
 const ListaDeGastos = () => {
 const [gastos] = useObtenerGastos();
+
+const formatearFecha = (fecha) =>{
+  return format(fromUnixTime(fecha), "dd 'de' MMMM 'de' yyyy", {locale: es});
+}
+
+const fechaEsIgual = (gastos, index, gasto) =>{
+  if(index !== 0){
+    const fechaActual = formatearFecha(gasto.fecha);
+    const fechaGastoAnterior = formatearFecha(gastos[index -1].fecha);
+    if(fechaActual === fechaGastoAnterior){
+      return true;
+    }else{
+      return false;
+    }
+  }
+}
   return (
     <>
       <Helmet>
@@ -42,21 +60,25 @@ const [gastos] = useObtenerGastos();
         <Titulo>Lista de Gastos</Titulo>
       </Header>
       <Lista>
-        {gastos.map((gasto)=>{
+        {gastos.map((gasto, index)=>{
           return(
-            <ElementoLista key={gasto.id}>
-              <Categoria><IconoCategoria id={gasto.categoria}/>{gasto.categoria}</Categoria>
-              <Descripcion>{gasto.descripcion}</Descripcion>
-              <Valor>{ConvertirAMoneda(gasto.cantidad)}</Valor>
-              <ContenedorBotones>
-                <BotonAccion as={Link} to={`/editar/${gasto.id}`}>
-                  <IconoEditar />
-                </BotonAccion>
-                <BotonAccion >
-                  <IconoBorrar />
-                </BotonAccion>
-              </ContenedorBotones>
-            </ElementoLista>
+            <div key={gasto.id}>
+              {!fechaEsIgual(gastos, index, gasto) && <Fecha>{formatearFecha(gasto.fecha)}</Fecha>}
+              
+              <ElementoLista key={gasto.id}>
+                <Categoria><IconoCategoria id={gasto.categoria}/>{gasto.categoria}</Categoria>
+                <Descripcion>{gasto.descripcion}</Descripcion>
+                <Valor>{ConvertirAMoneda(gasto.cantidad)}</Valor>
+                <ContenedorBotones>
+                  <BotonAccion as={Link} to={`/editar/${gasto.id}`}>
+                    <IconoEditar />
+                  </BotonAccion>
+                  <BotonAccion >
+                    <IconoBorrar />
+                  </BotonAccion>
+                </ContenedorBotones>
+              </ElementoLista>
+            </div>
           );
         })}
         <ContenedorBotonCentral>
